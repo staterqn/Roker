2fa680025168c435303d1b75fa117ceb69259ce4
chuanshuangzhang chuan shuang <chuanshuangzhangchuanshuang@chuanshuangzhangdeMac-mini.local>
2015-04-07 10:45:45 +0800

    Initial Commit

---
+++ Roker/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Roker
+//
+
+//  Copyright (c) 2015年 chuanshaungzhang. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
